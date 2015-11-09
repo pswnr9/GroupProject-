a42f0d4759ab8e88966d46ff77b82dac2eb68df2
Tianbo Wang <twn64@mail.missouri.edu>
2015-11-09 01:27:06 -0600

    secondform added

--- application/controllers/forms.php
+++ application/controllers/forms.php
@@ -17,5 +17,21 @@ class Forms extends CI_Controller {
           $this->load->view('pages/'.$page, $data);
           $this->load->view('templates/footer', $data);
         }
+        
+        public function form($page = 'secondform')
+        {
+          if ( ! file_exists(APPPATH.'/views/users/'.$page.'.php'))
+          {
+            // Whoops, we don't have a page for that!
+            show_404();
+          }
+
+          $data['title'] = ucfirst($page); // Capitalize the first letter
+$this->load->helper('form');          
+$this->load->helper('url');
+          $this->load->view('templates/header', $data);
+          $this->load->view('users/'.$page, $data);
+          $this->load->view('templates/footer', $data);
+        }
 }
 ?>
