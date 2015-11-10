ea2f4be5bf741460fef012eaf0c286a7295c8b77
dbmfzf <dbmfzf@mail.missouri.edu>
2015-11-09 20:25:34 -0600

    did work on the account model

--- application/models/account.php
+++ application/models/account.php
@@ -8,9 +8,26 @@ class Account extends CI_model {
 
     //Returns true if authentication is successful, returns false if not
     public function authentication($data){
-        //$password_hash = pull salt and password hash from database
-
-
+        //Querying the database
+        $this->db->select('salt, password');
+        $this->db->where('pawprint', $data["pawprint"]);
+        //switch case changes the table the data is retrieved from based on user type
+        switch ($data["user_type"]){
+            case "employee":
+                $query = $this->db->get('emp_user_info');
+                break;
+            case "admin":
+                $query = $this->db->get('admin_user_info');
+                break;
+            case "tech":
+                $query = $this->db->get('tech_user_info');
+                break;
+            default:
+                break;
+        }
+        $row = $query->first_row('array');
+        $password_hash = $row['password'];
+        $salt = $row['salt'];
         if($password_hash == hash('sha512', $data["password"] . $salt, false)){
             return true;
         } else{
@@ -18,29 +35,24 @@ class Account extends CI_model {
         }
     }
 
-    //More parameters will need to be added
-    public function registerAccount($userInfo){
+    public function registerEmployeeAccount($userInfo){
         foreach ($userInfo as $key => $value) {
             $userInfo[$key] = htmlspecialchars($value);
         }
-        //Store username, password, salt, etc in DB
+        mt_srand();
+        $userInfo['salt'] = mt_rand();
+        $userInfo['password'] = hash('sha512', $userInfo['password'] . $userInfo['salt'], false);
+        $this->db->insert('emp_user_info', $userInfo); 
     }
-
-    //In the same boat as registerAccount
-    public function createAdministration($username, $password){
-        $cleanUsername = htmlspecialchars($username);
-        $cleanPass = htmlspecialchars($password);
-        $salt = mt_rand();
-        $password_hash = hash('sha512', $cleanPass . $salt, false);
-
-        //Store username, password, salt, etc in DB
-    }
-
-    //returns the user type for determining access level
-    public function getUserType($username){
-        $cleanUsername = htmlspecialchars($username);
-        //Get $usertype from the database using the given username
-        return $userType;
+    
+        public function registerAdminAccount($userInfo){
+        foreach ($userInfo as $key => $value) {
+            $userInfo[$key] = htmlspecialchars($value);
+        }
+        mt_srand();
+        $userInfo['salt'] = mt_rand();
+        $userInfo['password'] = hash('sha512', $userInfo['password'] . $userInfo['salt'], false);
+        $this->db->insert('admin_user_info', $userInfo); 
     }
 
 }
