d61e4dfa012b6c84b59d8d449a77fa6c4380f307
dbmfzf <dbmfzf@mail.missouri.edu>
2015-11-20 21:40:11 -0600

    tweaked the database a little bit, changed form 1 so administrators can view it if it gets the information sent to it

--- application/views/forms/form_1.php
+++ application/views/forms/form_1.php
@@ -35,59 +35,59 @@
 
     <div class="pure-u-1-3">
       <label for="username">Username</label>
-      <input type="text" name="username" id="username" class="pure-u-23-24" value="<?php echo isset($username) ? $username : ""; ?>">
+      <input <?php echo isset($adminView) ? "readonly" : "" ; ?> type="text" name="username" id="username" class="pure-u-23-24" value="<?php echo isset($username) ? $username : ""; ?>">
     </div>
 
       <div class="pure-u-1-3">
       <label for="title">Title</title>
-      <input type="text" name="title"  id="title" class="pure-u-23-24" value="<?php echo isset($title) ? $title : ""; ?>">
+      <input <?php echo isset($adminView) ? "readonly" : "" ; ?> type="text" name="title"  id="title" class="pure-u-23-24" value="<?php echo isset($title) ? $title : ""; ?>">
     </div>
 
     <div class="pure-u-1-3">
       <label for="department">Department</label>
-      <input type="text" name="organization" id="department" class="pure-u-23-24" value="<?php echo isset($organization) ? $organization : ""; ?>">
+      <input <?php echo isset($adminView) ? "readonly" : "" ; ?> type="text" name="organization" id="department" class="pure-u-23-24" value="<?php echo isset($organization) ? $organization : ""; ?>">
     </div>
 
     <div class="pure-u-1-3">
       <label for="pawprint">Pawprint</label>
-      <input type="text" name="pawprint" id="pawprint"  class="pure-u-23-24" value="<?php echo $_SESSION['pawprint']; ?>" disabled>
+      <input <?php echo isset($adminView) ? "readonly" : "" ; ?> type="text" name="pawprint" id="pawprint"  class="pure-u-23-24" value="<?php echo $_SESSION['pawprint']; ?>" disabled>
     </div>
 
       <div class="pure-u-1-3">
       <label for="emplid">EmplID</label>
-      <input type="number" name="empiid" id="emplid" class="pure-u-23-24" value="<?php echo isset($empiid) ? $empiid : ""; ?>">
+      <input <?php echo isset($adminView) ? "readonly" : "" ; ?> type="number" name="empiid" id="emplid" class="pure-u-23-24" value="<?php echo isset($empiid) ? $empiid : ""; ?>">
     </div>
 
       <div class="pure-u-1-3">
       <label for="campus">Campus Address</label>
-      <input type="text" name="address" id="campus"  class="pure-u-23-24" value="<?php echo isset($address) ? $address : ""; ?>">
+      <input <?php echo isset($adminView) ? "readonly" : "" ; ?> type="text" name="address" id="campus"  class="pure-u-23-24" value="<?php echo isset($address) ? $address : ""; ?>">
     </div>
 
 
       <div class="pure-u-1-3">
       <label for="number">Phone Number</label>
-      <input type="text" name="phone_num" id="number" class="pure-u-23-24" value="<?php echo isset($phone_num) ? $phone_num : ""; ?>">
+      <input <?php echo isset($adminView) ? "readonly" : "" ; ?> type="text" name="phone_num" id="number" class="pure-u-23-24" value="<?php echo isset($phone_num) ? $phone_num : ""; ?>">
     </div>
     </div>
       <hr>
 
       <h3>Security Information</h3>
       <label for="new" class="pure-checkbox">
-      <input id="new" type="radio" name="request_status" value="0" checked="checked"> New Request
-      <input id="additional" type="radio" name="request_status" value="1"> Additional Request
+      <input <?php echo isset($adminView) ? "disabled" : "" ; ?> id="new" type="radio" name="request_status" value="0" checked="checked"> New Request
+      <input <?php echo isset($adminView) ? "disabled" : "" ; ?> id="additional" type="radio" name="request_status" value="1"> Additional Request
     </label>
 
 
     <label id="studentworker" for="remember" class="pure-checkbox">
-      <input id="studentworker" type="checkbox" name="student_worker" value="1"> Student Worker
+      <input <?php echo isset($adminView) ? "disabled" : "" ; ?> id="studentworker" type="checkbox" name="student_worker" value="1"> Student Worker
     </label>
 
       <h4>Copy Security of a Staff Member (Choose Current or Former)</h4>
       <div class="pure-u-1-3">
       <label for="current" class="pure-checkbox">
-      <input id="current" type="radio" name="if_cur_staff" value="2"> Current
-      <input id="former" type="radio" name="if_cur_staff" value="1"> Former
-      <input id="n_f_c" type="radio" name="if_cur_staff" value="0" checked="checked"> None
+      <input <?php echo isset($adminView) ? "disabled" : "" ; ?> id="current" type="radio" name="if_cur_staff" value="2"> Current
+      <input <?php echo isset($adminView) ? "disabled" : "" ; ?> id="former" type="radio" name="if_cur_staff" value="1"> Former
+      <input <?php echo isset($adminView) ? "disabled" : "" ; ?> id="n_f_c" type="radio" name="if_cur_staff" value="0" checked="checked"> None
       </label>
     </div>
 
@@ -97,22 +97,22 @@
 
        <div class="pure-u-1-3">
       <label for="name">Name:</label>
-      <input type="text" name="ref_name" id="name" class="pure-u-23-24" value="none">
+      <input <?php echo isset($adminView) ? "readonly" : "" ; ?> type="text" name="ref_name" id="name" class="pure-u-23-24" value="none">
     </div>
 
       <div class="pure-u-1-3">
       <label for="position">Position:</label>
-      <input type="text" name="ref_pos" id="position" class="pure-u-23-24" value="none">
+      <input <?php echo isset($adminView) ? "readonly" : "" ; ?> type="text" name="ref_pos" id="position" class="pure-u-23-24" value="none">
     </div>
 
       <div class="pure-u-1-3">
       <label for="sso">SSO/Pawprint:</label>
-      <input type="text" name="ref_pawprint" id="sso" class="pure-u-23-24" value="none">
+      <input <?php echo isset($adminView) ? "readonly" : "" ; ?> type="text" name="ref_pawprint" id="sso" class="pure-u-23-24" value="none">
     </div>
 
       <div class="pure-u-1-3">
       <label for="emplid2">EmplID:</label>
-      <input type="number" name="ref_empiid" id="emplid2" class="pure-u-23-24" value="0000">
+      <input <?php echo isset($adminView) ? "readonly" : "" ; ?> type="number" name="ref_empiid" id="emplid2" class="pure-u-23-24" value="0000">
     </div>
       <hr>
 
@@ -122,28 +122,28 @@
       </p>
       <div class="pure-u-1-3">
       <label for="ferpa">Ferpa Score:</label>
-      <input type="number" name="ferpa_score" id="ferpa" class="pure-u-23-24" min="85" max="100">
+      <input <?php echo isset($adminView) ? "readonly" : "" ; ?> type="number" name="ferpa_score" id="ferpa" class="pure-u-23-24" min="85" max="100">
     </div>
       <hr>
 
       <p>Select the Academic Career(s). Please check all that apply.<p>
-      <input type="checkbox" name="ac[]" value="4">UGRD
-      <input type="checkbox" name="ac[]" value="3">GRAD
-      <input type="checkbox" name="ac[]" value="2">MED
-      <input type="checkbox" name="ac[]" value="1">VET MED
-      <input type="checkbox" name="ac[]" value="0">LAW
+      <input <?php echo isset($adminView) ? "disabled" : "" ; ?> type="checkbox" name="ac[]" value="4">UGRD
+      <input <?php echo isset($adminView) ? "disabled" : "" ; ?> type="checkbox" name="ac[]" value="3">GRAD
+      <input <?php echo isset($adminView) ? "disabled" : "" ; ?> type="checkbox" name="ac[]" value="2">MED
+      <input <?php echo isset($adminView) ? "disabled" : "" ; ?> type="checkbox" name="ac[]" value="1">VET MED
+      <input <?php echo isset($adminView) ? "disabled" : "" ; ?> type="checkbox" name="ac[]" value="0">LAW
       <br>
       <hr>
 
       <p>Which type of access do you need?</p>
-      <input type="radio" name="access_type" value="2" checked="checked">Student Records
-      <input type="radio" name="access_type" value="3">Admissions
-      <input type="radio" name="access_type" value="4">Student Financials
-      <input type="radio" name="access_type" value="5">Student Financial Aid
-      <input type="radio" name="access_type" value="6">Reserved Access
+      <input <?php echo isset($adminView) ? "disabled" : "" ; ?> type="radio" name="access_type" value="2" <?php echo isset($adminView) ? "" : 'checked="checked"' ; echo isset($access_type)&&$access_type == 2 ? 'checked="checked"' : "" ; ?>>Student Records
+      <input <?php echo isset($adminView) ? "disabled" : "" ; ?> type="radio" name="access_type" value="3" <?php echo isset($access_type)&&$access_type == 3 ? 'checked="checked"' : "" ; ?>>Admissions
+      <input <?php echo isset($adminView) ? "disabled" : "" ; ?> type="radio" name="access_type" value="4" <?php echo isset($access_type)&&$access_type == 4 ? 'checked="checked"' : "" ; ?>>Student Financials
+      <input <?php echo isset($adminView) ? "disabled" : "" ; ?> type="radio" name="access_type" value="5" <?php echo isset($access_type)&&$access_type == 5 ? 'checked="checked"' : "" ; ?>>Student Financial Aid
+      <input <?php echo isset($adminView) ? "disabled" : "" ; ?> type="radio" name="access_type" value="6" <?php echo isset($access_type)&&$access_type == 6 ? 'checked="checked"' : "" ; ?>>Reserved Access
       <hr>
 
-      <button class="button" type="submit">Submit</button>
+      <?php echo isset($adminView) ? "" : '<button class="button" type="submit">Submit</button>' ;?>
     </div>
 
 
